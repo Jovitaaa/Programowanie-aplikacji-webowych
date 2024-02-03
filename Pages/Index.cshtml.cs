b61acb44f0494d15c71751c34df0feba011f5142
Jovitaaa <137905779+Jovitaaa@users.noreply.github.com>
2024-02-03 22:36:05 +0100

    Add files via upload

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+using WebApplication2.Models;
+
+namespace WebApplication2.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        private readonly IItemRepository _itemRepository;
+
+        public IndexModel(ILogger<IndexModel> logger, IItemRepository itemRepository)
+        {
+            _logger = logger;
+            _itemRepository = itemRepository;
+        }
+
+        public void OnGet()
+        {
+            Items = _itemRepository.GetItems();
+            Regions = _itemRepository.GetRegions();
+        }
+    }
+}
